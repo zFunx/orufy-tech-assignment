3efc4df6df00107a04c7e4c638a7d51413f083a4
zFunx <20762882+zFunx@users.noreply.github.com>
2023-05-18 13:01:57 +0530

    Add categries

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,15 +1,26 @@
 import Link from 'next/link'
-import logo from '@/assets/mini-trips.com.png'
 import Search from './Search'
+
+// Assets
+import logo from '@/assets/mini-trips.com.png'
+import { AiOutlineUnorderedList } from 'react-icons/ai'
+
 
 function Nav() {
     return (
         <div className="shadow-xl p-3">
-            <div className="max-w-4xl mx-auto flex">
-                <Link href="https://mini-trips.com" target="_blank"><img alt="logo" src={logo.src} className="h-8" /></Link>
-            <div className="ml-12">
-                <Search />
-            </div>
+            <div className="max-w-4xl mx-auto flex justify-between items-center">
+                <div className='flex'>
+                    <Link href="https://mini-trips.com" target="_blank"><img alt="logo" src={logo.src} className="h-8" /></Link>
+                    <div className="ml-12">
+                        <Search />
+                    </div>
+                </div>
+                <div className='flex'>
+                    <strong className="inline-flex gap-1 items-center cursor-pointer">
+                        <AiOutlineUnorderedList /> Categories
+                    </strong>
+                </div>
             </div>
         </div>
     )
